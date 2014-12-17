a57bf22ef2b61dc26d56aa76cfbce29a6a3581c0
Stuart Douglas <stuart.w.douglas@gmail.com>
2014-12-17 14:11:01 +1100

    Fix JSP issue

--- src/main/java/io/undertow/jsp/JspFileHandler.java
+++ src/main/java/io/undertow/jsp/JspFileHandler.java
@@ -4,9 +4,10 @@ import io.undertow.server.HandlerWrapper;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.servlet.handlers.ServletRequestContext;
-import org.apache.jasper.Constants;
 
 import javax.servlet.ServletRequest;
+
+import static org.apache.jasper.Constants.JSP_FILE;
 
 public class JspFileHandler implements HttpHandler {
 
@@ -21,12 +22,16 @@ public class JspFileHandler implements HttpHandler {
     @Override
     public void handleRequest(final HttpServerExchange exchange) throws Exception {
         ServletRequest request = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY).getServletRequest();
-        Object old = request.getAttribute(Constants.JSP_FILE);
+        Object old = request.getAttribute(JSP_FILE);
         try {
-            request.setAttribute(Constants.JSP_FILE, jspFile);
+            if(jspFile == null) {
+                request.removeAttribute(JSP_FILE);
+            } else {
+                request.setAttribute(JSP_FILE, jspFile);
+            }
             next.handleRequest(exchange);
         } finally {
-            request.setAttribute(Constants.JSP_FILE, old);
+            request.setAttribute(JSP_FILE, old);
         }
     }
 
