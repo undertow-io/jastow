32fd19f60804ae3074d1a5708c17aabf42897386
Richard Opálka <opalka.richard@gmail.com>
2020-09-11 10:02:35 +0200

    [UNDERTOW-1768] Introducing java.* class names constants

--- src/main/java/org/apache/jasper/compiler/Constants.java
+++ src/main/java/org/apache/jasper/compiler/Constants.java
@@ -26,6 +26,45 @@ final class Constants {
         // forbidden instantiation
     }
 
+    // java.beans
+    private static final String PACKAGE_JAVA_BEANS = "java.beans";
+    static final String BEANS = PACKAGE_JAVA_BEANS + ".Beans";
+
+    // java.io
+    private static final String PACKAGE_JAVA_IO = "java.io";
+    static final String IO_EXCEPTION = PACKAGE_JAVA_IO + ".IOException";
+    static final String WRITER = PACKAGE_JAVA_IO + ".Writer";
+    static final String STRING_READER = PACKAGE_JAVA_IO + ".StringReader";
+    static final String STRING_WRITER = PACKAGE_JAVA_IO + ".StringWriter";
+
+    // java.lang
+    private static final String PACKAGE_JAVA_LANG = "java.lang";
+    static final String BOOLEAN = PACKAGE_JAVA_LANG + ".Boolean";
+    static final String BYTE = PACKAGE_JAVA_LANG + ".Byte";
+    static final String CHARACTER = PACKAGE_JAVA_LANG + ".Character";
+    static final String CLASS = PACKAGE_JAVA_LANG + ".Class";
+    static final String CLASS_NOT_FOUND_EXCEPTION = PACKAGE_JAVA_LANG + ".ClassNotFoundException";
+    static final String DOUBLE = PACKAGE_JAVA_LANG + ".Double";
+    static final String EXCEPTION = PACKAGE_JAVA_LANG + ".Exception";
+    static final String FLOAT = PACKAGE_JAVA_LANG + ".Float";
+    static final String ILLEGAL_STATE_EXCEPTION = PACKAGE_JAVA_LANG + ".IllegalStateException";
+    static final String INSTANTIATION_EXCEPTION = PACKAGE_JAVA_LANG + ".InstantiationException";
+    static final String INTEGER = PACKAGE_JAVA_LANG + ".Integer";
+    static final String LONG = PACKAGE_JAVA_LANG + ".Long";
+    static final String OBJECT = PACKAGE_JAVA_LANG + ".Object";
+    static final String SHORT = PACKAGE_JAVA_LANG + ".Short";
+    static final String STRING = PACKAGE_JAVA_LANG + ".String";
+    static final String THROWABLE = PACKAGE_JAVA_LANG + ".Throwable";
+    static final String VOID = PACKAGE_JAVA_LANG + ".Void";
+
+    // java.util
+    private static final String PACKAGE_JAVA_UTIL = "java.util";
+    static final String ARRAY_LIST = PACKAGE_JAVA_UTIL + ".ArrayList";
+    static final String HASH_MAP = PACKAGE_JAVA_UTIL + ".HashMap";
+    static final String HASH_SET = PACKAGE_JAVA_UTIL + ".HashSet";
+    static final String MAP = PACKAGE_JAVA_UTIL + ".Map";
+    static final String SET = PACKAGE_JAVA_UTIL + ".Set";
+
     // javax.el
     private static final String PACKAGE_JAVAX_EL = "javax.el";
     static final String EXPRESSION_FACTORY = PACKAGE_JAVAX_EL + ".ExpressionFactory";
