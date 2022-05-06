97a7fb1d1f96169202c89b41cb070089d26a4dc8
Richard Opálka <opalka.richard@gmail.com>
2022-05-06 14:26:29 +0200

    [UNDERTOW-2100][UNDERTOW-2052] Upgrading Jastow to Servlet API 6.0.0, JSP API 3.1.0 and Undertow 2.3.0.Alpha1. Also switching by default to Jakarta namespace only.

---
+++ src/main/java/io/undertow/jsp/JspFileHandler.java
@@ -0,0 +1,46 @@
+package io.undertow.jsp;
+
+import io.undertow.server.HandlerWrapper;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.servlet.handlers.ServletRequestContext;
+
+import jakarta.servlet.ServletRequest;
+
+import static org.apache.jasper.Constants.JSP_FILE;
+
+public class JspFileHandler implements HttpHandler {
+
+    private final String jspFile;
+    private final HttpHandler next;
+
+    public JspFileHandler(final String jspFile, final HttpHandler next) {
+        this.jspFile = jspFile;
+        this.next = next;
+    }
+
+    @Override
+    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+        ServletRequest request = exchange.getAttachment(ServletRequestContext.ATTACHMENT_KEY).getServletRequest();
+        Object old = request.getAttribute(JSP_FILE);
+        try {
+            if(jspFile == null) {
+                request.removeAttribute(JSP_FILE);
+            } else {
+                request.setAttribute(JSP_FILE, jspFile);
+            }
+            next.handleRequest(exchange);
+        } finally {
+            request.setAttribute(JSP_FILE, old);
+        }
+    }
+
+    public static HandlerWrapper jspFileHandlerWrapper(final String jspFile) {
+        return new HandlerWrapper() {
+            @Override
+            public HttpHandler wrap(HttpHandler handler) {
+                return new JspFileHandler(jspFile, handler);
+            }
+        };
+    }
+}
