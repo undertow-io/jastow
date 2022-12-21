d2e14208022212a05100cc6a702e158a53997372
rmartinc <rmartinc@redhat.com>
2022-12-21 11:55:53 +0100

    [UNDERTOW-2214] Jastow compilation error when mixing EL and scriptlet expressions after UNDERTOW-1319

---
+++ src/test/java/io/undertow/test/jsp/taglib/MyOutTag.java
@@ -0,0 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.undertow.test.jsp.taglib;
+
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+/**
+ *
+ * @author rmartinc
+ */
+public class MyOutTag extends SimpleTagSupport {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        getJspContext().getOut().print(value);
+    }
+}
