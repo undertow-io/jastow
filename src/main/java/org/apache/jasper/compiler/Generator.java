203efc10bfb6652b921386ccb379d5830583ddd6
baranowb <baranowb@gmail.com>
2025-07-11 12:03:30 +0200

    [UNDERTOW-2549] Revert "Merge pull request #105 from ropalka/UNDERTOW-2401"
    This reverts commit 48bd7a51a854a5ded5cc3ed5bf7848b142ee8528, reversing
    changes made to f5ceadfccf8dd49e691c2103b0df8fc3f126590a.

--- src/main/java/org/apache/jasper/compiler/Generator.java
+++ src/main/java/org/apache/jasper/compiler/Generator.java
@@ -2184,10 +2184,6 @@ class Generator {
                 writeNewInstance(tagHandlerVar, tagHandlerClassName);
             }
 
-            // Wrap use of tag in try/finally to ensure clean-up takes place
-            out.printil("try {");
-            out.pushIndent();
-
             // includes setting the context
             generateSetters(n, tagHandlerVar, handlerInfo, false);
 
@@ -2351,6 +2347,18 @@ class Generator {
             out.print(tagHandlerVar);
             printlnThreePart(out, ".doEndTag() == ", TAG, ".SKIP_PAGE) {");
             out.pushIndent();
+            if (!n.implementsTryCatchFinally()) {
+                if (isPoolingEnabled && !(n.implementsJspIdConsumer())) {
+                    out.printin(n.getTagHandlerPoolName());
+                    out.print(".reuse(");
+                    out.print(tagHandlerVar);
+                    out.println(");");
+                } else {
+                    out.printin(tagHandlerVar);
+                    out.println(".release();");
+                    writeDestroyInstance(tagHandlerVar);
+                }
+            }
             if (isTagFile || isFragment) {
                 printilThreePart(out, "throw new ", SKIP_PAGE_EXCEPTION, "();");
             } else {
@@ -2383,15 +2391,6 @@ class Generator {
                 out.println(".doFinally();");
             }
 
-            if (n.implementsTryCatchFinally()) {
-                out.popIndent();
-                out.printil("}");
-            }
-
-            // Ensure clean-up takes place
-            out.popIndent();
-            out.printil("} finally {");
-            out.pushIndent();
             if (isPoolingEnabled && !(n.implementsJspIdConsumer())) {
                 out.printin(n.getTagHandlerPoolName());
                 out.print(".reuse(");
@@ -2403,8 +2402,10 @@ class Generator {
                 writeDestroyInstance(tagHandlerVar);
             }
 
-            out.popIndent();
-            out.printil("}");
+            if (n.implementsTryCatchFinally()) {
+                out.popIndent();
+                out.printil("}");
+            }
 
             // Declare and synchronize AT_END scripting variables (must do this
             // outside the try/catch/finally block)
