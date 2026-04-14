da17253f016317b7900525d200effcc7b9acf49e
Flavia Rainone <frainone@redhat.com>
2026-04-14 05:16:42 -0300

    [UNDERTOW-2748] Refactor. Simplify 'Map' operations. No functional change.
    Based on: https://github.com/apache/tomcat/commit/3e62a4f13bd1fc04b3e6d89988135c5c90fe2c1c
    
    Signed-off-by: Flavia Rainone <frainone@redhat.com>

--- src/main/java/org/apache/jasper/compiler/PageInfo.java
+++ src/main/java/org/apache/jasper/compiler/PageInfo.java
@@ -331,12 +331,8 @@ class PageInfo {
      * @param uri The URI to be pushed onto the stack
      */
     public void pushPrefixMapping(String prefix, String uri) {
-        LinkedList<String> stack = xmlPrefixMapper.get(prefix);
-        if (stack == null) {
-            stack = new LinkedList<>();
-            xmlPrefixMapper.put(prefix, stack);
-        }
-        stack.addFirst(uri);
+        // Must be LinkedList as it needs to accept nulls
+        xmlPrefixMapper.computeIfAbsent(prefix, k -> new LinkedList<>()).addFirst(uri);
     }
 
     /*
